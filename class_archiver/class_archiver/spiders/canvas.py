ef816896a1ef399526d57df2c5bdf7448b1b0a57
Spencer Pogorzelski <34356756+spencerpogo@users.noreply.github.com>
2024-12-20 15:37:26 -0500

    wip downloading files works

--- class_archiver/class_archiver/spiders/canvas.py
+++ class_archiver/class_archiver/spiders/canvas.py
@@ -2,6 +2,8 @@ import os
 import re
 import scrapy
 
+from ..items import CanvasFileItem
+
 
 # taken from https://github.com/psf/requests/blob/23540c93cac97c763fe59e843a08fa2825aa80fd/src/requests/utils.py#L917
 def parse_header_links(value):
@@ -48,7 +50,7 @@ class CanvasModulesSpider(scrapy.Spider):
         super(CanvasModulesSpider, self).__init__(*args, **kwargs)
         self.allowed_domains = [self.canvas_domain]
         self.token = os.environ["CANVAS_TOKEN"]
-    
+
     @classmethod
     def update_settings(cls, settings):
         super().update_settings(settings)
@@ -71,7 +73,8 @@ class CanvasModulesSpider(scrapy.Spider):
 
     def start_requests(self):
         yield self.canvas_request(
-            self.canvas_endpoint(f"courses/{self.course_id}/modules"), self.parse_modules_list
+            self.canvas_endpoint(f"courses/{self.course_id}/modules"),
+            self.parse_modules_list,
         )
 
     def parse_modules_list(self, response):
@@ -94,7 +97,8 @@ class CanvasModulesSpider(scrapy.Spider):
             if ty == "File":
                 yield self.canvas_request(it["url"], self.parse_file)
             else:
-                raise NotImplementedError(f"Item type {ty!r} not implemented")
+                # raise NotImplementedError(f"Item type {ty!r} not implemented")
+                continue
 
         links = parse_header_links(response.headers.get("link", "").decode())
         next_links = [l for l in links if l.get("rel") == "next"]
@@ -103,16 +107,48 @@ class CanvasModulesSpider(scrapy.Spider):
             yield self.canvas_request(
                 next_links[0]["url"],
                 self.parse_module_items,
-                cb_kwargs={"module": module}
+                cb_kwargs={"module": module},
             )
-    
+
     def parse_file(self, response):
-        raise NotImplementedError()
-    
+        f = response.json()
+        assert isinstance(f, dict), f"expected dict from file response, got {f!r}"
+        assert (
+            "url" in f
+        ), f"missing download url: {__import__('json').dumps(f, indent=4)}"
+        return CanvasFileItem(
+            course_id=self.course_id,
+            id=f["id"],
+            # in general "filename" is the URL-encoded version of "display_name"
+            # we'll change the semantics slightly for our purposes (we are fine with
+            #  storing filenames with spaces on disk)
+            filename=f["display_name"],
+            download_url=f["url"],
+        )
+
     def parse_assignment(self, response):
         assignment = response.json()
         desc = Selector(text=assignment["description"])
         for f in desc.css(".instructure_file_link"):
             # could just fetch the URL in data-api-endpoint and pass it to parse_file
             #  but we can save a request by parsing the HTML attributes directly
-            
+            assert (
+                f.attrib["data-api-returntype"] == "File"
+            ), f"unexpected data-api-returntype: {f.attrib!r}"
+            course_id = assignment["course_id"]
+            endpoint_parts = assignment["data-api-endpoint"].split(
+                f"/courses/{course_id}/files/"
+            )
+            assert (
+                len(endpoint_parts) == 2
+            ), f"unexpected data-api-endpoint format: {assignment['data-api-endpoint']!r}"
+            _, file_id = endpoint_parts
+            file_id = int(file_id)
+            filename = f.attrib["title"]
+            download_url = f.attrib["href"]
+            yield CanvasFileItem(
+                course_id=course_id,
+                id=file_id,
+                filename=filename,
+                download_url=download_url,
+            )
